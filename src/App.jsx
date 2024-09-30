1312fae997b09e7285a3b3ef1571ca8951a6c527
Meesam Zaidi <meesam.engineer@gmail.com>
2024-09-30 10:11:34 +0530

    working on chips filter row

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,12 @@ function App () {
       <header className='col-span-2'>
         <Header />
       </header>
-      <div className='col-span-2 row-start-2 row-end-5 auto-rows-max bg-red-500'>
-        Body
+      <div className='col-span-2 row-start-2 row-end-5 auto-rows-max'>
+        <div className='grid grid-rows-[4rem, 1fr] grid-flow-row grid-cols-5'>
+          <div className='col-span-5 bg-blue-500 px-5 md:px-10'>
+            Hello
+          </div>
+        </div>
       </div>
       <div className='col-span-2 h-20 row-span-1 bg-blue-600'>
         Footer
