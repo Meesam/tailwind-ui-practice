6b16e97c9963732c715e9b5cf5ab0aedde2fe27e
Meesam <meesam.engineer@gmail.com>
2025-03-21 09:35:32 +0530

    Frontend mentor Challange 1 is done

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,9 @@
-import FilterRail from './components/FilterRail'
-import Header from './components/Header'
-import CardList from './components/CardList'
+import ChallangeOne from './frontendmentorchallange/challange1'
 
 function App () {
   return (
-    <div className='w-screen h-screen bg-gray-50 grid grid-rows-[4rem,1fr] grid-flow-row grid-cols-2 overflow-y-auto'>
-      <header className='col-span-2'>
-        <Header />
-      </header>
-      <div className='col-span-2 row-start-2 row-end-3 px-5 md:px-10 h-full my-4'>
-        <CardList />
-      </div>
+    <div className='h-screen bg-gray-900 justify-center items-center flex'>
+      <ChallangeOne />
     </div>
   )
 }
